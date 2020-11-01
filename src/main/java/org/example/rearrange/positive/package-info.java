4771ceef6bc02a847f8d9bef9778a09d09b03668
Jesper Olsson <jesper@jesperolsson.se>
2020-11-01 12:07:51 +0100

    #6 Describe the object composition approach

---
+++ src/main/java/org/example/rearrange/positive/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a snap-shot of our module, for when it catered to two
+ * user segments (FizzBuzz and BuzzFizz). It is implemented through object
+ * composition and decorators.
+ */
+package org.example.rearrange.positive;
